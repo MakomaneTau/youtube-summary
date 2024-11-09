0c29a72772e9bcc4c2e39250987571235368ef0f
MakomaneTau <151774523+MakomaneTau@users.noreply.github.com>
2024-11-09 21:26:09 +0200

    change

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname,'public/index.html'));
 })
+
+app.use((req, res) =>{
+    res.status(404);
+    res.send('<h1>Error 404:Resource not found</h1>');
+})
+
+app.listen(3000, ()=>{
+    console.group("App listening on port 3000");
+})
