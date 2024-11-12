65f6a71f8698d6d141501210361aa5bf6b15cfbb
MakomaneTau <151774523+MakomaneTau@users.noreply.github.com>
2024-11-12 23:52:06 +0200

    ohk

--- new/app.js
+++ new/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+console.log("hello");
+
 app.use(express.static(path.join(__dirname , 'public')));
 
 app.get('/', (req, res) => {
